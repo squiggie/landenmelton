c230af6fb87603c9d2972d27de2ba3e12cae225f
Squiggie <squigge@gmail.com>
2017-11-21 21:15:31 -0600

    add sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,6 @@ export class AppComponent {
   position: string;
   description: Array<String> = [];
   projects: Array<any> = [];
-  project = {
-    img: '',
-    title: '',
-    description: '',
-    facebook: '',
-    twitter: '',
-    linkedin: '',
-    link: ''
-  };
 
   constructor() {
     // setup descriptions
@@ -26,11 +17,17 @@ export class AppComponent {
     this.description.push('I have extensive experience building and maintaining back end systems for financial instututions. I\'ve worked with and supported datacenter and cloud infrastructure systems for years. If your system run Windows or Linux, I\'ve likely dealt with it before. From Apache to Nginx, Powershell to Bash, Java to Javascript, I\'ve probably had to hack on it or design and implement full systems with it.');
     this.description.push('My most recent passion is designing and building mobile and web applications using Wordpress, HTML, CSS and Javascript; specifically dealing with frameworks like Angular and Ionic. If you\'re looking for a developer with extensive experience, that can clearly communicte needs, specifications, deadlines, cost and more, you\'ve found him. I\'d love to speak with you about your next project.');
 
-    const project1 = this.project;
-    project1.img = 'assets/devotable-min.png';
-    project1.title = 'Devotable';
-    project1.description = 'Devotable is a daily devotion website and mobile application that allows users to read and study daily Bible devotions from various authors.';
-    project1.link = 'http://devotableapp.com';
-    this.projects.push(project1);
+    this.projects.push({
+      img: 'assets/devotable_logo_black_large_no_text.png',
+      title: 'Devotable',
+      description: 'Devotable is a daily devotion website and mobile application that allows users to read and study daily Bible devotions from various authors.',
+      link: 'https://devotableapp.com',
+    });
+    this.projects.push({
+      img: 'assets/bb-logo.png',
+      title: 'The Blockchain Boom',
+      description: 'The Blockchain Boom is a website and blog dedicated to explaining, reviewing and recommending products and services around Bitcoin and blockchain.',
+      link: 'https://theblockchainboom.com',
+    });
   }
 }
